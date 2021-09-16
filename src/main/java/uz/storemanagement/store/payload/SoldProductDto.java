c6dc71f1d6ad4a7eafc4696320c94b458cc2fafd
maraim tuxtasunov <maraimtuxtasunov@gmail.com>
2021-09-16 11:36:39 +0500

    fixed some bugs

--- src/main/java/uz/storemanagement/store/payload/SoldProductDto.java
+++ src/main/java/uz/storemanagement/store/payload/SoldProductDto.java
@@ -1,12 +1,17 @@
-package uz.storemanagement.store.payload;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class SoldProductDto {
-
-}
+//package uz.storemanagement.store.payload;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//public class SoldProductDto {
+//
+//    private Long id;
+//    private Long productId;
+//}
